8ff58b70a9509f091c4774461acf4ac4f28c5f8c
nordyjay <norton.justin.a@gmail.com>
2025-10-10 16:48:17 -0400

    facelift-2

--- apps/documents/urls.py
+++ apps/documents/urls.py
@@ -5,4 +5,6 @@ app_name = 'documents'
 
 urlpatterns = [
     path('upload/', views.upload_document, name='upload'),
+    path('view/<int:document_id>/', views.view_document, name='view'),
+    path('download/<int:document_id>/', views.download_document, name='download'),
 ]
